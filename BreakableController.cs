39f7aae515b85ca6723dc29cb3118276223e5922
Tomos Slater <Tomos.Slater@outlook.com>
2019-02-11 21:23:34 +0000

    Version 0.6.6:
    	- Added script to make breakables actually breakable!
    	- Breakables now play a breaking animation when something collides with them
    	- Breakables will also despawn 5 seconds after breaking

---
+++ BreakableController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableController : MonoBehaviour
+{
+    [SerializeField] private Animator animator; //animator of the breakable
+    [SerializeField] private Collider col; //collider of the breakable
+
+    //Controls what happens when something collides with the breakable
+    //In that even, plays the break animation, turns the collider off, 
+    //and destroys the object after 5 seconds
+    private void OnCollisionEnter(Collision collision) {
+        col.enabled = false;
+        animator.SetBool("Breaking", true);
+        Destroy(gameObject, 5f);
+    }
+}
